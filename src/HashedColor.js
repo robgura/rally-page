4c1bc468c62aaf4783db672f6571a22ad409562d
Rob Gura <rob.gura@vorne.com>
2025-07-16 13:26:53 -0500

    special hash color for api and web text

--- src/HashedColor.js
+++ src/HashedColor.js
@@ -12,6 +12,13 @@ export default function HashedColor(props) {
     } = props;
 
     let hsh = simpleHash(text) % 360;
+    if (text === 'api') {
+        hsh = 90;
+    }
+    else if (text === 'web') {
+        hsh = 210;
+    }
+
     const _lightness = lightness || '20%';
     const style = {
         backgroundColor: `hsl(${hsh}, 100%, ${_lightness})`,
